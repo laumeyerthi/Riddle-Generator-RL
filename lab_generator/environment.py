4aeeb216e5c29c41229f1f2d433b233d0db65d38
bakethi <konstantin.bake@thi.de>
2025-02-16 08:55:05 +0100

    press_button logic

--- lab_generator/environment.py
+++ lab_generator/environment.py
@@ -37,11 +37,11 @@ class Environment():
         # get the indices of the buttons in the current room
         self.dynamic_action_space.append([np.where(self.lab.button_location_matrix[self.current_room] == 1)[0].tolist()])
         #get the indices of the possible room transtions in the current room
-        room_transitons = np.where(self.lab.door_state_matrix[self.current_room] == 1)[0]
+        room_transitions = np.where(self.lab.door_state_matrix[self.current_room] == 1)[0]
         # remove the current room
-        room_transitons = room_transitons[room_transitons != self.current_room]
+        room_transitions = room_transitions[room_transitions != self.current_room]
         # append to possible move throu door actions to the action space
-        self.dynamic_action_space.append([room_transitons.tolist()])
+        self.dynamic_action_space.append([room_transitions.tolist()])
         # append the backtrack action
         if self.last_room is not None:
             self.dynamic_action_space.append([self.backtrack])
@@ -54,7 +54,13 @@ class Environment():
             return False
         
     def press_button(self, button):
-        pass
+        # which doors have to change
+        door_indices = np.where((self.lab.button2door_behavior_matrix[button] == 1)[0] & (np.arange(self.lab.button2door_behavior_matrix.shape[0]) != button))[0]
+        # remove doors that are not connected to another room
+        door_indices = door_indices[self.lab.room_trans_matrix[door_indices, button] != 0]
+        # switch the door states
+        self.lab.door_state_matrix[door_indices, button] = 1 - self.lab.door_state_matrix[door_indices, button]
+        
 
     def move(self, door):
         self.last_room = self.current_room
